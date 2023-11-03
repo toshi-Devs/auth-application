14e68331eb0c60ef357ae703cd917a735e3cefb5
toshi-Devs <trahim87mustapha@gmail.com>
2023-11-03 21:51:48 +0200

    delete unused variables in App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,10 @@ import { useAuth } from './context/AuthContext';
 
 function App() {
   const [showLogin, setShowLogin] = useState(true);
-  const [userLoggedIn, setUserLoggedIn] = useState(false); // State variable to track user login status
   const { currentUser } = useAuth();
 
   const toggleForm = () => {
     setShowLogin(!showLogin);
-  };
-
-  const handleUserLogin = () => {
-    setUserLoggedIn(true); // Set userLoggedIn to true when the user logs in or registers
   };
 
   return (
@@ -23,20 +18,11 @@ function App() {
       {currentUser ? (
         <Home />
       ) : showLogin ? (
-        <Login toggleForm={toggleForm} onUserLogin={handleUserLogin} />
+        <Login toggleForm={toggleForm}  />
       ) : (
-        <Signup toggleForm={toggleForm} onUserLogin={handleUserLogin} />
+        <Signup toggleForm={toggleForm}  />
       )}
 
-      {/* <Home /> */}
-      {/* <Login /> */}
-      {/* {userLoggedIn ? (
-        <Home /> // Render the Home component when the user is logged in
-      ) : showLogin ? (
-        <Login toggleForm={toggleForm} onUserLogin={handleUserLogin} /> // Pass onUserLogin as a callback to Login
-      ) : (
-        <Signup toggleForm={toggleForm} onUserLogin={handleUserLogin} /> // Pass onUserLogin as a callback to Signup
-      )} */}
     </div>
   );
 }
